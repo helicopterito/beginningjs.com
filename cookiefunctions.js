8b714186f2f62f98c5be0f65c47e8a516044c8dd
Jeremy McPeak <jwmcpeak@gmail.com>
2014-08-17 15:30:08 -0500

    added ch13 ex1

---
+++ cookiefunctions.js
@@ -0,0 +1,15 @@
+﻿function setCookie(name, value, path, expires) {
+    value = escape(value);
+
+    if (!expires) {
+        var now = new Date();
+        now.setMonth(now.getMonth() + 6);
+        expires = now.toUTCString();
+    }
+
+    if (path) {
+        path = ";Path=" + path;
+    }
+
+    document.cookie = name + "=" + value + ";expires=" + expires + path;
+}
