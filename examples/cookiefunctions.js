75b57c7896b06f53a63fae81a57517d6cf4dbaee
Jeremy McPeak <jwmcpeak@gmail.com>
2014-08-23 17:38:19 -0500

    updated examples for ch13

--- examples/cookiefunctions.js
+++ examples/cookiefunctions.js
@@ -13,3 +13,29 @@
 
     document.cookie = name + "=" + value + ";expires=" + expires + path;
 }
+
+function getCookieValue(name) {
+    var value = document.cookie;
+    var cookieStartsAt = value.indexOf(" " + name + "=");
+
+    if (cookieStartsAt == -1) {
+        cookieStartsAt = value.indexOf(name + "=");
+    }
+
+    if (cookieStartsAt == -1) {
+        value = null;
+    } else {
+        cookieStartsAt = value.indexOf("=", cookieStartsAt) + 1;
+
+        var cookieEndsAt = value.indexOf(";", cookieStartsAt);
+
+        if (cookieEndsAt == -1) {
+            cookieEndsAt = value.length;
+        }
+
+        value = unescape(value.substring(cookieStartsAt,
+            cookieEndsAt));
+    }
+
+    return value;
+}
