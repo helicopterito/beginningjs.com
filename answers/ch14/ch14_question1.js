c2f81cdac1dad274b0c1c1bf7800ba3eba91ea78
Jeremy McPeak <jwmcpeak@gmail.com>
2014-12-08 01:37:59 -0600

    added answers to several questions

---
+++ answers/ch14/ch14_question1.js
@@ -0,0 +1,34 @@
+﻿function HttpRequest(url, callback) {
+    this.url = url;
+    this.callBack = callback;
+    this.async = true;
+    this.request = new XMLHttpRequest();
+};
+
+HttpRequest.prototype.send = function() {
+    this.request.open("GET", this.url, this.async);
+
+    if (this.async) {
+        var tempRequest = this.request;
+        var callback = this.callBack;
+
+        function requestReadystatechange() {
+            if (tempRequest.readyState == 4) {
+                if (tempRequest.status == 200) {
+                    callback(tempRequest.responseText);
+                } else {
+                    alert("An error occurred while attempting to " +
+                        "contact the server.");
+                }
+            }
+        }
+
+        this.request.onreadystatechange = requestReadystatechange;
+    }
+
+    this.request.send(null);
+
+    if (!this.async) {
+        this.callBack(this.request.responseText);
+    }
+};
